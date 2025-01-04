41b7d2a71462608811aa5baa96303eb6c2664182
rotosiri-zu <hongjitufang25@gmail.com>
2025-01-04 21:20:07 +0900

    @NotBlank,Sizeの追記

--- src/main/java/com/example/webapp/form/ToDoForm.java
+++ src/main/java/com/example/webapp/form/ToDoForm.java
@@ -1,5 +1,7 @@
 package com.example.webapp.form;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,8 +16,10 @@ public class ToDoForm {
 	/** することID */
 	private Integer id;
 	/** すること */
+	@NotBlank(message = "ToDoは必勝です。")
 	private String todo;
 	/** すること詳細 */
+	@Size(min = 1, max = 100, message = "詳細は{min}～{max}文字以内で入力してください。")
 	private String detail;
 	/** 新規判定 */
 	private Boolean isNew;
