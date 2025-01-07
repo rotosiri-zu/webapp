e0af107bf2de54774477d24f78b8ed61835da0cf
rotosiri-zu <hongjitufang25@gmail.com>
2025-01-07 16:10:33 +0900

    passwwwordEncoderの修正

--- src/main/java/com/example/webapp/config/PasswordConfig.java
+++ src/main/java/com/example/webapp/config/PasswordConfig.java
@@ -2,7 +2,7 @@ package com.example.webapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -10,6 +10,6 @@ public class PasswordConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		// エンコードの設定
-		return NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
 	}
 }
